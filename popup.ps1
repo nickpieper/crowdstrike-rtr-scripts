107fbe17eb6652b10877df87840950b61acec63d
Nick Pieper <56167458+nickpieper@users.noreply.github.com>
2023-03-17 10:29:28 -0400

    Add files via upload

---
+++ popup.ps1
@@ -0,0 +1,6 @@
+﻿$Message = -join  
+(  
+ "Your system, $env:computername, has been quarantined by our anti-virus solution. Please reach out to the CTG Help Desk and inform them of this message. The CTG US Help Desk can be reached at 1-800-544-9071, and the CTG EU Helpdesk can be reached at +32 2 718 9120."  
+)  
+$strCmd = "c:\windows\system32\msg.exe * " + $Message  
+Invoke-Expression $strCmd
